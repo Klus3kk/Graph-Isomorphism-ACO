3ea1e0658808c9b01915bec4430e861f16d15780
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-11-04 22:27:48 +0100

    Created a graph generator.

--- main.py
+++ main.py
@@ -16,11 +16,27 @@ class Graph:
         for row in self.adjacency_matrix:
             print(row)
 
+    def generate_random_graph(self, edge_probability=0.3, max_weight=10):
+        """
+        Generuje losowy graf. 
+        Parametry:
+        - edge_probability: prawdopodobieństwo utworzenia krawędzi między dwoma wierzchołkami
+        - max_weight: maksymalna wartość wagi krawędzi
+        """
+        for i in range(self.num_vertices):
+            for j in range(i + 1, self.num_vertices):
+                if random.random() <= edge_probability:
+                    weight = random.randint(1, max_weight)
+                    self.add_edge(i, j, weight)
+
+
+
 # Przykład użycia
 g = Graph(5)
-g.add_edge(0, 1, 4)
-g.add_edge(1, 2, 5)
-g.add_edge(2, 3, 2)
-g.add_edge(3, 4, 1)
-g.add_edge(4, 0, 3)
+# g.add_edge(0, 1, 4)
+# g.add_edge(1, 2, 5)
+# g.add_edge(2, 3, 2)
+# g.add_edge(3, 4, 1)
+# g.add_edge(4, 0, 3)
+g.generate_random_graph(edge_probability=0.5, max_weight=15)
 g.display()
